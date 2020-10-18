69b8e0a1962f6d93a616c57a2c73c90466e9aa12
Asato Nago <nago@tam-bourine.co.jp>
2020-10-19 01:03:49 +0900

    feat: 🎸 groupize-new も globalMenu と同様に

---
+++ functions/src/slack/blocks/new-modal.ts
@@ -0,0 +1,70 @@
+import { SystemConst } from "../../const";
+import { View } from "../../types/blocks/modal";
+import { ViewsOpenArguments } from "@slack/web-api/dist/methods";
+
+export const getView = ({ context, trigger_id }: View): ViewsOpenArguments => {
+  return {
+    token: context.botToken,
+    trigger_id: trigger_id as string,
+    view: {
+      type: "modal",
+      callback_id: SystemConst.GroupizeNew.VIEW_ID,
+      title: {
+        type: "plain_text",
+        text: "groupize new",
+      },
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "enter group name, select users",
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "input",
+          block_id: "group_name",
+          element: {
+            action_id: "group_name_action",
+            type: "plain_text_input",
+          },
+          label: {
+            type: "plain_text",
+            text: "Group name",
+            emoji: true,
+          },
+        },
+        {
+          type: "input",
+          block_id: "group_users",
+          element: {
+            type: "multi_users_select",
+            action_id: "multi_users_select_action",
+            placeholder: {
+              type: "plain_text",
+              text: "Select users :woman-boy: ",
+              emoji: true,
+            },
+          },
+          label: {
+            type: "plain_text",
+            text: "Group users",
+            emoji: true,
+          },
+        },
+      ],
+      submit: {
+        type: "plain_text",
+        text: "Register",
+      },
+      close: {
+        type: "plain_text",
+        text: "Cancel",
+        emoji: true,
+      },
+    },
+  };
+};
--- functions/src/slack/commands/groupize-new.ts
+++ functions/src/slack/commands/groupize-new.ts
@@ -1,81 +1,18 @@
 import { App } from "@slack/bolt";
 import { modalArgs } from "../../types/common";
 import * as db from "../../db/putGroup"
+import { SystemConst } from "../../const";
+import * as newModal from "../blocks/new-modal"
 
-const VIEW_ID = "dialog_3";
-
-const openModal = async ({ app, trigger_id, context }: modalArgs) => {
-  await app.client.views.open({
-    token: context.botToken,
-    trigger_id,
-    view: {
-      type: "modal",
-      callback_id: VIEW_ID,
-      title: {
-        type: "plain_text",
-        text: "groupize new",
-      },
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "enter group name, select users",
-          },
-        },
-        {
-          type: "divider",
-        },
-        {
-          type: "input",
-          block_id: "group_name",
-          element: {
-            action_id: "group_name_action",
-            type: "plain_text_input",
-          },
-          label: {
-            type: "plain_text",
-            text: "Group name",
-            emoji: true,
-          },
-        },
-        {
-          type: "input",
-          block_id: "group_users",
-          element: {
-            type: "multi_users_select",
-            action_id: "multi_users_select_action",
-            placeholder: {
-              type: "plain_text",
-              text: "Select users :woman-boy: ",
-              emoji: true,
-            },
-          },
-          label: {
-            type: "plain_text",
-            text: "Group users",
-            emoji: true,
-          },
-        },
-      ],
-      submit: {
-        type: "plain_text",
-        text: "Register",
-      },
-      close: {
-        type: "plain_text",
-        text: "Cancel",
-        emoji: true,
-      },
-    },
-  });
+export const openRegisterModal = async ({ app, trigger_id, context }: modalArgs) => {
+  await app.client.views.open(newModal.getView({ context, trigger_id }));
 };
 
 export const useGroupizeNewCommand = (app: App) => {
-  app.command("/groupize-new", async ({ ack, body, context, command }) => {
+  app.command(SystemConst.GroupizeNew.COMMAND_NAME, async ({ ack, body, context, command }) => {
     await ack();
     try {
-      await openModal({
+      await openRegisterModal({
         app,
         trigger_id: body.trigger_id,
         context,
@@ -86,7 +23,7 @@ export const useGroupizeNewCommand = (app: App) => {
     }
   });
 
-  app.view(VIEW_ID, async ({ ack, view, context, body }) => {
+  app.view(SystemConst.GroupizeNew.VIEW_ID, async ({ ack, view }) => {
     await ack();
     const channelId = view.private_metadata;
     const values = view.state.values;
